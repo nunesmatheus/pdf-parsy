fd3729660aa9b95f4048237a61d19e36ee71a8b7
Matheus Nunes <matheus.o.nunes@gmail.com>
2020-08-16 17:19:29 -0300

    Fix controller name

--- app/controllers/pdf_images_controller.rb
+++ app/controllers/pdf_images_controller.rb
@@ -1,4 +1,4 @@
-class PdfToTextController < ApplicationController
+class PdfImagesController < ApplicationController
   def create
     images = ImagesFromPdfService.call(params[:file].path)
     render json: { images: images }
