5c4a1976325d278e093edb7239817738eee40d36
Matheus Nunes <matheus.o.nunes@gmail.com>
2021-01-13 16:03:40 -0300

    Refactor pdftotext code

--- handler.py
+++ handler.py
@@ -1,12 +1,9 @@
 import json
 import boto3
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
 import fitz
 import os
+import sys
+from pdf_to_text import convert as convert_to_text
 
 
 def pdf_to_text(event, context):
@@ -15,7 +12,7 @@ def pdf_to_text(event, context):
         return missing_key_response()
 
     path = download_file(key)
-    text = convert_pdf_to_txt(path)
+    text = convert_to_text(path)
 
     response = {
         "statusCode": 200,
@@ -28,6 +25,7 @@ def pdf_to_text(event, context):
     }
 
     return response
+
 
 def pdf_images(event, context):
     key = file_key(event)
@@ -45,9 +43,9 @@ def pdf_images(event, context):
         for img in doc.getPageImageList(i):
             xref = img[0]
             pix = fitz.Pixmap(doc, xref)
-            if pix.n - pix.alpha < 4: # this is GRAY or RGB
+            if pix.n - pix.alpha < 4:  # this is GRAY or RGB
                 pix.writePNG("%s/p%s-%s.png" % (images_directory, i, xref))
-            else: # CMYK: convert to RGB first
+            else:  # CMYK: convert to RGB first
                 pix1 = fitz.Pixmap(fitz.csRGB, pix)
                 pix1.writePNG("%s/p%s-%s.png" % (images_directory, i, xref))
                 pix1 = None
@@ -73,33 +71,9 @@ def pdf_images(event, context):
     return response
 
 
-def convert_pdf_to_txt(path):
-    # TODO: Should extract to another file
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    laparams = LAParams()
-    # Default LAParams: char_margin=2.0, line_margin=0.5, word_margin=0.1 all_texts=False
-    laparams.line_margin = 20.0
-    laparams.char_margin = 10.0
-    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    maxpages = 0
-    caching = True
-    pagenos = set()
-
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
-
 def file_key(event):
     return event.get('queryStringParameters', {}).get('key')
+
 
 def missing_key_response():
     {
@@ -107,12 +81,15 @@ def missing_key_response():
         "body": "Missing 'key' query parameter to fetch from S3"
     }
 
+
 def download_file(key):
     s3 = boto3.client('s3')
     path = '/tmp/file.pdf'
     s3.download_file(os.environ.get('S3_BUCKET'), key, path)
     return path
 
+
 def upload_file(key, file_name):
     s3 = boto3.client('s3')
-    response = s3.upload_file(file_name, os.environ.get('S3_BUCKET'), key, ExtraArgs={'ACL': 'public-read'})
+    response = s3.upload_file(file_name, os.environ.get(
+        'S3_BUCKET'), key, ExtraArgs={'ACL': 'public-read'})
---
+++ pdf_to_text.py
@@ -0,0 +1,38 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+
+def convert(path):
+    file = open(path, 'rb')
+    text = __process_pages(file)
+    file.close()
+    return text
+
+
+def __process_pages(file):
+    retstr = StringIO()
+    resource_manager = PDFResourceManager()
+    interpreter = __interpreter(resource_manager, retstr)
+    for page in PDFPage.get_pages(fp=file, pagenos=set(), maxpages=0, caching=True, check_extractable=True):
+        interpreter.process_page(page)
+    interpreter.device.close()
+    text = retstr.getvalue()
+    retstr.close()
+    return text
+
+
+def __interpreter(resource_manager, retstr):
+    device = TextConverter(resource_manager, retstr, codec='utf-8',
+                           laparams=__layout_params())
+    return PDFPageInterpreter(rsrcmgr=resource_manager, device=device)
+
+
+def __layout_params():
+    # Default LAParams: char_margin=2.0, line_margin=0.5, word_margin=0.1 all_texts=False
+    laparams = LAParams()
+    laparams.line_margin = 20.0
+    laparams.char_margin = 10.0
+    return laparams
