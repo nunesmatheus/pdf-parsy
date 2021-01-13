e8a76427ea6ce88bd4646593acc608e016639562
Matheus Nunes <matheus.o.nunes@gmail.com>
2021-01-13 12:08:45 -0300

    Add endpoint to parse pdf_images

--- handler.py
+++ handler.py
@@ -5,19 +5,17 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
+import fitz
+import os
+import base64
 
 
-def pdftotext(event, context):
-    key = event.get('queryStringParameters', {}).get('key')
+def pdf_to_text(event, context):
+    key = file_key(event)
     if key == None:
-        return {
-            "statusCode": 200,
-            "body": "Missing 'key' query parameter to fetch from S3"
-        }
+        return missing_key_response()
 
-    s3 = boto3.client('s3')
-    path = '/tmp/file.pdf'
-    s3.download_file('vstiba-provas', key, path)
+    path = download_file(key)
     text = convert_pdf_to_txt(path)
 
     response = {
@@ -25,6 +23,47 @@ def pdftotext(event, context):
         "body": text,
         "headers": {
             "Content-Type": "text/plain; charset=utf-8"
+        }
+    }
+
+    return response
+
+def pdf_images(event, context):
+    key = file_key(event)
+    if key == None:
+        return missing_key_response()
+
+    path = download_file(key)
+    doc = fitz.open(path)
+    images_directory = '/tmp/images'
+    if not os.path.exists(images_directory):
+        os.mkdir(images_directory)
+
+    for i in range(len(doc)):
+        for img in doc.getPageImageList(i):
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            if pix.n - pix.alpha < 4:       # this is GRAY or RGB
+                pix.writePNG("%s/p%s-%s.png" % (images_directory, i, xref))
+            else:               # CMYK: convert to RGB first
+                pix1 = fitz.Pixmap(fitz.csRGB, pix)
+                pix1.writePNG("%s/p%s-%s.png" % (images_directory, i, xref))
+                pix1 = None
+            pix = None
+
+    images = []
+    for filename in os.listdir(images_directory):
+        with open("%s/%s" % (images_directory, filename), "rb") as image_file:
+            encoded = base64.b64encode(image_file.read()).decode('ascii')
+            images.append(encoded)
+
+    response = {
+        "statusCode": 200,
+        "body": {
+            "images": images
+        },
+        "headers": {
+            "Content-Type": "application/json"
         }
     }
 
@@ -54,3 +93,18 @@ def convert_pdf_to_txt(path):
     device.close()
     retstr.close()
     return text
+
+def file_key(event):
+    return event.get('queryStringParameters', {}).get('key')
+
+def missing_key_response():
+    {
+        "statusCode": 200,
+        "body": "Missing 'key' query parameter to fetch from S3"
+    }
+
+def download_file(key):
+    s3 = boto3.client('s3')
+    path = '/tmp/file.pdf'
+    s3.download_file(os.environ.get('S3_BUCKET'), key, path)
+    return path
