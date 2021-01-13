e71609305bd5228acb33116080d69f089964d9ab
Matheus Nunes <matheus.o.nunes@gmail.com>
2021-01-13 07:59:24 -0300

    Add real pdftotext endpoint implementation

--- handler.py
+++ handler.py
@@ -1,24 +1,53 @@
 import json
+import boto3
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
 
 
 def pdftotext(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    key = event.get('queryStringParameters', {}).get('key')
+    if key == None:
+        return {
+            "statusCode": 200,
+            "body": "Missing 'key' query parameter to fetch from S3"
+        }
+
+    s3 = boto3.client('s3')
+    path = '/tmp/file.pdf'
+    s3.download_file('vstiba-provas', key, path)
+    text = __convert_pdf_to_txt(path)
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": text,
+        "headers": {
+            "Content-Type": "text/plain; charset=utf-8"
+        }
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def __convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
