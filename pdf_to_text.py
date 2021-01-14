fd032f8cb1a622ee53b022707e47b2937ac0e5a1
Matheus Nunes <matheus.o.nunes@gmail.com>
2021-01-14 08:10:43 -0300

    Refactor pdf_to_text into class

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -5,34 +5,38 @@ from pdfminer.pdfpage import PDFPage
 from io import StringIO
 
 
-def convert(path):
-    file = open(path, 'rb')
-    text = __process_pages(file)
-    file.close()
-    return text
+class PdfToText:
+    def __init__(self, path):
+        self.path = path
+
+    def convert(self):
+        file = open(self.path, 'rb')
+        text = self.__process_pages(file)
+        file.close()
+        return text
 
 
-def __process_pages(file):
-    retstr = StringIO()
-    resource_manager = PDFResourceManager()
-    interpreter = __interpreter(resource_manager, retstr)
-    for page in PDFPage.get_pages(fp=file, pagenos=set(), maxpages=0, caching=True, check_extractable=True):
-        interpreter.process_page(page)
-    interpreter.device.close()
-    text = retstr.getvalue()
-    retstr.close()
-    return text
+    def __process_pages(self, file):
+        retstr = StringIO()
+        resource_manager = PDFResourceManager()
+        interpreter = self.__interpreter(resource_manager, retstr)
+        for page in PDFPage.get_pages(fp=file, pagenos=set(), maxpages=0, caching=True, check_extractable=True):
+            interpreter.process_page(page)
+        interpreter.device.close()
+        text = retstr.getvalue()
+        retstr.close()
+        return text
 
 
-def __interpreter(resource_manager, retstr):
-    device = TextConverter(resource_manager, retstr, codec='utf-8',
-                           laparams=__layout_params())
-    return PDFPageInterpreter(rsrcmgr=resource_manager, device=device)
+    def __interpreter(self, resource_manager, retstr):
+        device = TextConverter(resource_manager, retstr, codec='utf-8',
+                               laparams=self.__layout_params())
+        return PDFPageInterpreter(rsrcmgr=resource_manager, device=device)
 
 
-def __layout_params():
-    # Default LAParams: char_margin=2.0, line_margin=0.5, word_margin=0.1 all_texts=False
-    laparams = LAParams()
-    laparams.line_margin = 20.0
-    laparams.char_margin = 10.0
-    return laparams
+    def __layout_params(self):
+        # Default LAParams: char_margin=2.0, line_margin=0.5, word_margin=0.1 all_texts=False
+        laparams = LAParams()
+        laparams.line_margin = 20.0
+        laparams.char_margin = 10.0
+        return laparams
--- test/unit/test_pdf_to_text.py
+++ test/unit/test_pdf_to_text.py
@@ -1,11 +1,12 @@
 import unittest
 import sys
-from pdf_to_text import convert
+from pdf_to_text import PdfToText
 
+PDF_FIXTURE_PATH = 'fixtures/sample.pdf'
 
 class TestPdfToText(unittest.TestCase):
     def test_text_extraction(self):
-        text = convert('fixtures/sample.pdf')
+        text = PdfToText(PDF_FIXTURE_PATH).convert()
         self.assertTrue(text.find('A Simple PDF File') > 0)
         self.assertTrue(text.find('This is a small demonstration') > 0)
         self.assertTrue(text.find('Simple PDF File 2') > 0)
